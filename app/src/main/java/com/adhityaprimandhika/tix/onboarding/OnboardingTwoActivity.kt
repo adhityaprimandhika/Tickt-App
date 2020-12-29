5d779398193df17ef45678f10b7b2adf21f202e7
Adhitya Primandhika <59129295+adhityaprimandhika@users.noreply.github.com>
2020-12-29 13:25:45 +0700

    add splash screen, login screen, and sign up screen

---
+++ app/src/main/java/com/adhityaprimandhika/tix/onboarding/OnboardingTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.adhityaprimandhika.tix.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adhityaprimandhika.tix.R
+
+class OnboardingTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_two)
+    }
+}
