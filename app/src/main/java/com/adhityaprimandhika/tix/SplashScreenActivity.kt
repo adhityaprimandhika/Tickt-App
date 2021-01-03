75437918e7e702ece357153f7d7635931fe5bc9c
Adhitya Primandhika <59129295+adhityaprimandhika@users.noreply.github.com>
2021-01-03 17:45:50 +0700

    add history screen, splash screen activity, and adjust all the button

--- app/src/main/java/com/adhityaprimandhika/tix/SplashScreenActivity.kt
+++ app/src/main/java/com/adhityaprimandhika/tix/SplashScreenActivity.kt
@@ -1,11 +1,24 @@
 package com.adhityaprimandhika.tix
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.adhityaprimandhika.tix.onboarding.OnboardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, OnboardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
